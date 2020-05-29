4a2912c23d62615d1a1c60b3aa28b9adcf528c9f
designC <znehraks@gmail.com>
2020-05-29 23:30:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Management System
+
+본 프로젝트는 두번째 복습 관리시스템
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} alt="logo" />
+      <h2>make it</h2>
+    </div>
+  );
+}
+
+export default App;
